044713bef7802719caa86bb663a627d8bd4ae8a0
Tushar Konala <tusharkonala284@gmail.com>
2026-03-14 13:29:09 +0530

    refactor: refactor imports of WorkSection

--- components/WorkSection.tsx
+++ components/WorkSection.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
-import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 
 const projects = [
   {
